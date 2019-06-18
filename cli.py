e8a6eb977706b6d034d3e84c6055f4909d353a92
Javier Aguirre <contacto@javaguirre.net>
2019-06-18 19:19:57 +0200

    Logic separation, using f-strings

--- cli.py
+++ cli.py
@@ -1,3 +1,4 @@
+import re
 import os
 import json
 import subprocess
@@ -5,51 +6,53 @@ import subprocess
 import yaml
 import click
 
-TICK_CHARACTER = u'\u2713'
-ERROR_CHARACTER = u'\u2718'
+from constants import TICK_CHARACTER, ERROR_CHARACTER, SEPARATOR
+from services import render_apps, render_services
 
 
 @click.command()
 def onboarding():
-    """Simple aplication controlling you have al the tools needed for TNP"""
     config_path = os.environ.get('ONBOARDING_FILE_PATH', 'config.yml')
-    config = yaml.load(open(config_path, 'r'))
+    config = yaml.load(open(config_path, 'r'), Loader=yaml.FullLoader)
 
     shell = subprocess.check_output('echo $SHELL', shell=True)
     shell = shell.decode('utf-8').strip()
 
-    click.secho(config['title'])
-    click.secho(config['description'])
-    click.secho(config['description_apps'])
+    apps = '\n'.join(render_apps(config))
+    services = '\n'.join(render_services(config))
 
-    click.echo('-------------------------------------')
-    click.echo('You are using {} shell'.format(shell))
+    click.echo(f'''
+{config['title']}
 
-    for app, instructions in config['apps'].items():
-        try:
-            output = subprocess.check_output(
-                '{} --version'.format(app), shell=True)
-            output = output.decode('utf-8').strip()
+{SEPARATOR}
 
-            try:
-                version = output.split('version')[1]
-            except IndexError:
-                version = output
+{config['description']}
 
-            click.secho('{} {}: {}'.format(
-                TICK_CHARACTER, app, version), fg='green')
-        except subprocess.CalledProcessError as e:
-            click.secho('{} {}'.format(ERROR_CHARACTER, app), fg='red')
-            click.secho(instructions, fg='white')
+{config['description_apps']}
 
-    want_continue = click.confirm('Do you want to see the services you need access to?')
+{SEPARATOR}
 
-    if want_continue:
-        click.secho(config['description_services'])
-        [click.secho('{}\n\n{}\n'.format(service.capitalize(), instructions), fg='blue')
-        for service, instructions in config['services'].items()]
+You are using {shell} shell
 
-    click.secho(config['description_footer'])
+{apps}
+''')
+
+    want_continue = click.confirm(
+        'Do you want to see the services you need access to?')
+
+    click.secho(
+        f'''
+{SEPARATOR}
+
+{config['description_services']}
+
+{services}
+
+{SEPARATOR}
+
+{config['description_footer']}
+
+''')
 
 
 if __name__ == '__main__':
---
+++ constants.py
@@ -0,0 +1,3 @@
+TICK_CHARACTER = u'\u2713'
+ERROR_CHARACTER = u'\u2718'
+SEPARATOR = '---------------------------'
