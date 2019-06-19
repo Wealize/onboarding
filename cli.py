feea73c66695e78f82c9ecab36dab6f471929cbf
Javier Aguirre <contacto@javaguirre.net>
2019-06-19 10:30:11 +0200

    Separating formatters to another class

--- cli.py
+++ cli.py
@@ -6,8 +6,8 @@ import subprocess
 import yaml
 import click
 
-from constants import TICK_CHARACTER, ERROR_CHARACTER, SEPARATOR
 from services import render_apps, render_services
+from formatters import OnboardingFormatter
 
 
 @click.command()
@@ -15,44 +15,20 @@ def onboarding():
     config_path = os.environ.get('ONBOARDING_FILE_PATH', 'config.yml')
     config = yaml.load(open(config_path, 'r'), Loader=yaml.FullLoader)
 
+    formatter = OnboardingFormatter(config)
+
     shell = subprocess.check_output('echo $SHELL', shell=True)
     shell = shell.decode('utf-8').strip()
 
     apps = '\n'.join(render_apps(config))
     services = '\n'.join(render_services(config))
 
-    click.echo(f'''
-{config['title']}
-
-{SEPARATOR}
-
-{config['description']}
-
-{config['description_apps']}
-
-{SEPARATOR}
-
-You are using {shell} shell
-
-{apps}
-''')
-
+    click.echo(formatter.get_onboarding_text(shell, apps))
     want_continue = click.confirm(
         'Do you want to see the services you need access to?')
 
-    click.secho(
-        f'''
-{SEPARATOR}
-
-{config['description_services']}
-
-{services}
-
-{SEPARATOR}
-
-{config['description_footer']}
-
-''')
+    if want_continue:
+        click.echo(formatter.get_services_text(services))
 
 
 if __name__ == '__main__':
---
+++ formatters.py
@@ -0,0 +1,40 @@
+from constants import TICK_CHARACTER, ERROR_CHARACTER, SEPARATOR
+
+
+class OnboardingFormatter:
+    def __init__(self, config):
+        self.config = config
+
+    def get_onboarding_text(self, shell, apps):
+        config = self.config
+        return f'''
+{config['title']}
+
+{SEPARATOR}
+
+{config['description']}
+
+{config['description_apps']}
+
+{SEPARATOR}
+
+You are using {shell} shell
+
+{apps}
+'''
+
+    def get_services_text(self, services):
+        config = self.config
+
+        return f'''
+{SEPARATOR}
+
+{config['description_services']}
+
+{services}
+
+{SEPARATOR}
+
+{config['description_footer']}
+
+'''
